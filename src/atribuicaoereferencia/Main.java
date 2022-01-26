cf4ecc0c7b9417d89fd5de24f7eee316d735da4c
Davi Ramon <eudavi.ramon@gmail.com>
2022-01-26 20:15:56 -0300

    conceitos de atribuiçao de memoria

---
+++ src/atribuicaoereferencia/Main.java
@@ -0,0 +1,21 @@
+package atribuicaoereferencia;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println("IntA = " + intA + " intB = " + intB);
+        intA =2;
+        System.out.println("IntA = " + intA + " intB = " + intB); // continua atribuindo 1.
+
+        Objeto objetoA = new Objeto(1);
+        Objeto objetoB = objetoA;  // atribuo o valor de A em B // copia a referencia de memoria, aponta para o mesmo endereço de memória.
+        System.out.println("objetoA = " + objetoA + "objetoB = " + objetoB);
+
+
+        objetoA.setNum(2); // vai no endereço de memória e atribui o valor 2.   O dois objetos apontam para o mesmo valor de memória.
+        System.out.println("objetoA = " + objetoA + "objetoB = " + objetoB);
+    }
+}
