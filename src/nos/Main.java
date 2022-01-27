813b588b83b2904e30182c802a756327e1cfeed4
Davi Ramon <eudavi.ramon@gmail.com>
2022-01-26 21:32:40 -0300

    refatorado a classe Node, adicionando generics <T>

---
+++ src/nos/Main.java
@@ -0,0 +1,42 @@
+package nos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Node<String> node1 = new Node<>("Content Node 1");
+
+        Node<String> node2 = new Node<>("Content Node 2");
+
+        node1.setNextnode(node2); // o no 1 aponta para o 2...
+
+        Node<String> node3 = new Node<>("Content Node 3");
+        node2.setNextnode(node3); // o no 2 aponta para o 3..
+
+        Node<String> node4 = new Node<>("Content Node 4");
+        node3.setNextnode(node4); // o no3 aponta para o 4
+
+        //   node1 -> node2 -> node3 -> node4 -> null
+
+        System.out.println(node1);
+        System.out.println(node1.getNextnode()); // node 2
+
+        System.out.println(("________________________________"));
+
+        System.out.println(node1);
+        System.out.println(node1.getNextnode()); // node2
+        System.out.println(node1.getNextnode().getNextnode()); //node 3
+        System.out.println(node1.getNextnode().getNextnode().getNextnode()); // node 4
+        System.out.println(node1.getNextnode().getNextnode().getNextnode().getNextnode()); // null
+        System.out.println(node4.getNextnode()); // null
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/nos/Node.java
@@ -0,0 +1,37 @@
+package nos;
+
+public class Node<T> { // o <T> indica que a classe aceita valores de qualquer tipo
+
+   private T content; // o content pode ser genérico, qualquer TIPO.
+   private Node<T> nextnode; // o conteudo de todos os nós são do mesmo tipo T (genérico)
+
+   public Node(T content) {
+      this.nextnode = null; // referencia o nodo seguinte como nulo. no caso o ultimo.
+      this.content = content;
+   }
+
+   public T getContent() {
+      return content;
+   }
+
+   public void setContent(T content) {
+      this.content = content;
+   }
+
+   public Node getNextnode() {
+      return nextnode;
+   }
+
+   public void setNextnode(Node nextnode) {
+      this.nextnode = nextnode;
+   }
+
+
+   @Override
+   public String toString() {
+      return "Node{" +
+              "content='" + content + '\'' +
+              '}';
+   }
+
+}
