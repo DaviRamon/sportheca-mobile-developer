bcb0d622663d54961abbf690599ddfefd54614f4
Davi Ramon <eudavi.ramon@gmail.com>
2022-01-31 19:34:26 -0300

    implementando POO

---
+++ src/POO/Endereco.java
@@ -0,0 +1,17 @@
+package POO;
+
+public class Endereco {
+
+    public enum TipoEndereco {
+        RESIDENCIAL,
+        ENTREGA,
+        TRABALHO}
+
+    public String endereco;
+    public String numero;
+    public String complemento;
+    public String bairro;
+    public String estado;
+    public String cep;
+
+}
---
+++ src/POO/EntregaCartaoApp.java
@@ -0,0 +1,30 @@
+package POO;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class EntregaCartaoApp {
+
+    public static void main(String[] args) {
+
+        Endereco endereco = new Endereco();
+        endereco.cep = "000000";
+        // dados do endereço
+
+
+        Cliente cliente = new Cliente();
+        // dados do cliente
+
+        try {
+            cliente.adicionaEndereco(endereco);
+            System.out.println("Endereço adicionado");
+
+        } catch (Exception e) {
+            System.err.println("Houve um erro ao adicionar endereço " + e.getMessage());
+        }
+
+    }
+
+
+}
